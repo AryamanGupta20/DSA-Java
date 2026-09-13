34929a12b29b79487b204b92299b544e4fa59ea7
Aryaman Gupta <aryamangupta55@gmail.com>
2026-09-13 18:59:51 +0530

    Syntax

--- Functions/syntax.java
+++ Functions/syntax.java
@@ -1,13 +1,8 @@
 public class syntax{
     public static void main(String[] args) {
-        display add()
-        
-        {
-            System.out.print(a+b);
-        }
+     
         int a =5;
         int b=5;
-  display.add();
   
   
 }
