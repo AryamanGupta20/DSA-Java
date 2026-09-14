23331baaeb493e368f882c6d8c85c2214f30ad7e
Aryaman Gupta <aryamangupta55@gmail.com>
2026-09-14 21:41:58 +0530

    revision

--- Functions/BasicRevision/operators.java
+++ Functions/BasicRevision/operators.java
@@ -2,9 +2,12 @@ package BasicRevision;
 
 import java.util.Scanner;
 
-public class operators {
+public class operators 
+{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = 5;
+
     }
+
 }
